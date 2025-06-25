c4edf05d71acfbd1f19721b8bb9b1f75bb0ebbb5
lolearningcode <lolearningcode@gmail.com>
2025-06-25 15:53:04 -0700

    Create real model for Appointment

--- Fay/Fay/Models/Appointment.swift
+++ Fay/Fay/Models/Appointment.swift
@@ -8,19 +8,32 @@
 
 import Foundation
 
-struct Appointment: Identifiable {
-    let id: String
+struct AppointmentResponse: Codable {
+    let appointments: [Appointment]
+}
+
+struct Appointment: Codable, Identifiable {
+    var id: String { appointmentID }
+    
+    let appointmentID: String
+    let patientID: String
+    let providerID: String
+    let status: String
+    let appointmentType: String
     let start: Date
     let end: Date
-    let type: String
-    let provider: String
-
-    static func mockData() -> [Appointment] {
-        let formatter = ISO8601DateFormatter()
-        return [
-            Appointment(id: "1", start: formatter.date(from: "2025-11-08T19:00:00Z")!, end: formatter.date(from: "2025-11-08T20:00:00Z")!, type: "Follow-up", provider: "Jane Williams, RD"),
-            Appointment(id: "2", start: formatter.date(from: "2025-11-24T19:00:00Z")!, end: formatter.date(from: "2025-11-24T20:00:00Z")!, type: "Follow-up", provider: "Jane Williams, RD"),
-            Appointment(id: "3", start: formatter.date(from: "2025-12-16T19:00:00Z")!, end: formatter.date(from: "2025-12-16T20:00:00Z")!, type: "Follow-up", provider: "Jane Williams, RD")
-        ]
+    let durationInMinutes: Int
+    let recurrenceType: String
+    
+    enum CodingKeys: String, CodingKey {
+        case appointmentID = "appointment_id"
+        case patientID = "patient_id"
+        case providerID = "provider_id"
+        case status
+        case appointmentType = "appointment_type"
+        case start
+        case end
+        case durationInMinutes = "duration_in_minutes"
+        case recurrenceType = "recurrence_type"
     }
 }
