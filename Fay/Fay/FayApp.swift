a222b643c7053a87cac1d5b390514ded384cc03c
lolearningcode <lolearningcode@gmail.com>
2025-06-25 15:23:36 -0700

    Created project

---
+++ Fay/Fay/FayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FayApp.swift
+//  Fay
+//
+//  Created by Cleo Howard on 6/25/25.
+//
+
+import SwiftUI
+
+@main
+struct FayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
